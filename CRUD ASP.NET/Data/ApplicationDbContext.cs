64c74ee850a35b23dd30a8d0f872178b3965d27a
annieooijiayi <70126922+annieooijiayi@users.noreply.github.com>
2023-09-06 20:43:59 +0800

    Initial commit

---
+++ CRUD ASP.NET/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUD_ASP.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_ASP.NET.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+        
+        }
+
+        public DbSet<Category> Categories { get; set; }     
+    }
+}
